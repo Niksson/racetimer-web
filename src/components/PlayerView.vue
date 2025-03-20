e4c33a8e2e39a8df21e608a8c0a1c31f0ec58b90
Nikolay Masson <nick.masson@yandex.ru>
2025-03-20 23:00:34 +0100

    feat: implement a collapse variant using vue transitions

--- src/components/PlayerView.vue
+++ src/components/PlayerView.vue
@@ -13,6 +13,22 @@ function openStats() {
   console.log('open stats')
   statsOpen.value = true
 }
+
+function collapseBeforeEnter(el: Element) {
+  (el as HTMLElement).style.height = '0px'
+}
+
+function collapseEnter(el: Element) {
+  (el as HTMLElement).style.height = el.scrollHeight + 'px'
+}
+
+function collapseLeave(el: Element) {
+  (el as HTMLElement).style.height = el.scrollHeight + 'px'
+}
+
+function collapseAfterLeave(el: Element) {
+  (el as HTMLElement).style.height = ''
+}
 </script>
 
 <template>
@@ -21,17 +37,25 @@ function openStats() {
     <div class="grow flex flex-col justify-between relative">
       <div class="scramble">R' U' F U2 B2 D' R2 D R2 B2 U B2 D' B R' U2 F2 R2 B L F2 D2 U' R' U' F</div>
       <div class="time">0.000</div>
-      <div class="bg-base-300 rounded-t-(--radius-box) p-2 text-center" @click.stop="openStats">STATS</div>
-      <div v-if="statsOpen"
-        class="rounded-t-(--radius-box) absolute bottom-0 left-0 w-full max-h-max z-10 bg-base-300 p-4">
-        <button @click.stop="() => { }" class="w-full btn">SOLVES</button>
-        <div class="mt-2 text-left grid grid-flow-col grid-rows-3 gap-1">
-          <div>avg5</div>
-          <div>avg12</div>
-          <div>avg25</div>
-          <div>avg50</div>
-          <div>avg100</div>
-          <div>solves: 0/0</div>
+      <div id="stats-box" class="relative">
+        <div class="bg-base-300 rounded-t-(--radius-box) p-2 invisible" @click.stop="openStats">STATS</div>
+        <div class="bg-base-300 rounded-t-(--radius-box) p-2 absolute bottom-0 left-0 w-full z-10"
+          @click.stop="openStats">
+          <Transition name="stats-collapse" mode="out-in" @before-enter="collapseBeforeEnter" @enter="collapseEnter"
+            @leave="collapseLeave" @after-leave="collapseAfterLeave">
+            <div v-if="!statsOpen" class="text-center">STATS</div>
+            <div @click.stop="closeOpenedStats" v-else>
+              <button @click.stop="() => { }" class="w-full btn">SOLVES</button>
+              <div class="mt-2 text-left grid grid-flow-col grid-rows-3 gap-1">
+                <div>avg5</div>
+                <div>avg12</div>
+                <div>avg25</div>
+                <div>avg50</div>
+                <div>avg100</div>
+                <div>solves: 0/0</div>
+              </div>
+            </div>
+          </Transition>
         </div>
       </div>
     </div>

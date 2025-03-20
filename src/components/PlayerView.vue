1377fdc9052fe617dc27ccc048d61b762523b96f
Nikolay Masson <nick.masson@yandex.ru>
2025-03-21 00:44:00 +0100

    feat: improve collapse animation

--- src/components/PlayerView.vue
+++ src/components/PlayerView.vue
@@ -13,21 +13,24 @@ function openStats() {
   console.log('open stats')
   statsOpen.value = true
 }
+function toggleStats() {
+  console.log('toggle stats')
+  statsOpen.value = !statsOpen.value
+}
 
 function collapseBeforeEnter(el: Element) {
+  console.log('beforeEnter ' + el.scrollHeight);
   (el as HTMLElement).style.height = '0px'
 }
 
 function collapseEnter(el: Element) {
+  console.log('enter ' + el.scrollHeight);
   (el as HTMLElement).style.height = el.scrollHeight + 'px'
 }
 
 function collapseLeave(el: Element) {
-  (el as HTMLElement).style.height = el.scrollHeight + 'px'
-}
-
-function collapseAfterLeave(el: Element) {
-  (el as HTMLElement).style.height = ''
+  (el as HTMLElement).style.height = '0px';
+  (el as HTMLElement).style.overflow = 'hidden';
 }
 </script>
 
@@ -38,14 +41,12 @@ function collapseAfterLeave(el: Element) {
       <div class="scramble">R' U' F U2 B2 D' R2 D R2 B2 U B2 D' B R' U2 F2 R2 B L F2 D2 U' R' U' F</div>
       <div class="time">0.000</div>
       <div id="stats-box" class="relative">
-        <div class="bg-base-300 rounded-t-(--radius-box) p-2 invisible" @click.stop="openStats">STATS</div>
-        <div class="bg-base-300 rounded-t-(--radius-box) p-2 absolute bottom-0 left-0 w-full z-10"
-          @click.stop="openStats">
-          <Transition name="stats-collapse" mode="out-in" @before-enter="collapseBeforeEnter" @enter="collapseEnter"
-            @leave="collapseLeave" @after-leave="collapseAfterLeave">
-            <div v-if="!statsOpen" class="text-center">STATS</div>
-            <div @click.stop="closeOpenedStats" v-else>
-              <button @click.stop="() => { }" class="w-full btn">SOLVES</button>
+        <div class="bg-base-300 p-2 invisible">STATS</div>
+        <div class="bg-base-300 p-2 absolute bottom-0 left-0 w-full z-10" @click.stop="toggleStats">
+          <div class="text-center font-bold">STATS</div>
+          <Transition name="stats-collapse" @before-enter="collapseBeforeEnter" @enter="collapseEnter"
+            @leave="collapseLeave">
+            <div @click.stop="closeOpenedStats" v-if="statsOpen" class="">
               <div class="mt-2 text-left grid grid-flow-col grid-rows-3 gap-1">
                 <div>avg5</div>
                 <div>avg12</div>
@@ -54,6 +55,7 @@ function collapseAfterLeave(el: Element) {
                 <div>avg100</div>
                 <div>solves: 0/0</div>
               </div>
+              <button @click.stop="() => { }" class="w-full btn mt-2">SOLVES</button>
             </div>
           </Transition>
         </div>

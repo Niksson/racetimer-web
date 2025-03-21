2cd5a4066fb039eb5a85a64626385fbd08eb834c
Nikolay Masson <nick.masson@yandex.ru>
2025-03-21 22:09:17 +0100

    fix: fix broken scramble closing for p2

--- src/components/PlayerView.vue
+++ src/components/PlayerView.vue
@@ -4,32 +4,20 @@ import { useTemplateRef } from 'vue';
 import StatsCollapse from './StatsCollapse.vue';
 const collapse = useTemplateRef('collapse')
 
-import TwoSideModal from './TwoSideModal.vue';
+defineProps<{
+  scramble: string
+}>()
 
-const scrambleModal = useTemplateRef('scramble-modal')
-
-const scramble = "R' U' F U2 B2 D' R2 D R2 B2 U B2 D' B R' U2 F2 R2 B L F2 D2 U' R' U' F"
+defineEmits(['scramble-clicked'])
 </script>
 
 <template>
   <div @click="collapse?.close()" class="flex gap-3 flex-col">
     <div class=" score">0 : <span class="text-accent">0</span></div>
     <div class="grow flex flex-col justify-between relative">
-      <div class="scramble" @click="scrambleModal?.modal?.showModal()">{{ scramble }}</div>
+      <div class="scramble" @click="$emit('scramble-clicked')">{{ scramble }}</div>
       <div class="time">0.000</div>
       <StatsCollapse ref="collapse" />
     </div>
-    <TwoSideModal ref="scramble-modal">
-      <template #player2>
-        <div class="flex place-content-center">
-          <scramble-display event="333" :scramble="scramble" />
-        </div>
-      </template>
-      <template #player1>
-        <div class="flex place-content-center">
-          <scramble-display event="333" :scramble="scramble" />
-        </div>
-      </template>
-    </TwoSideModal>
   </div>
 </template>
--- src/components/TimerPage.vue
+++ src/components/TimerPage.vue
@@ -1,16 +1,35 @@
 <script setup lang="ts">
+import { useTemplateRef } from 'vue';
 import PlayerView from './PlayerView.vue';
+import TwoSideModal from './TwoSideModal.vue';
+const scramble = "R' U' F U2 B2 D' R2 D R2 B2 U B2 D' B R' U2 F2 R2 B L F2 D2 U' R' U' F"
+const scrambleModal = useTemplateRef('scramble-modal')
+function openScrambleModal() {
+  scrambleModal?.value?.modal?.showModal()
+}
 </script>
 
 <template>
   <div id="timer-page" class="unselectable grid grid-rows-[1fr_auto_1fr] h-screen w-full">
-    <PlayerView class="player2" />
+    <PlayerView @scramble-clicked="openScrambleModal" :scramble="scramble" class="player2" />
     <div class="divider-custom">
       <div class="divider-content absolute w-full -top-5 flex justify-between">
         <button class="ml-8 w-24 text-xl h-10 btn btn-outline bg-base-100">PUZZLES</button>
         <button class="mr-8 w-24 text-xl h-10 btn btn-outline bg-base-100">PENALTY</button>
       </div>
     </div>
-    <PlayerView />
+    <PlayerView @scramble-clicked="openScrambleModal" :scramble="scramble" />
+    <TwoSideModal ref="scramble-modal">
+      <template #player2>
+        <div class="flex place-content-center">
+          <scramble-display event="333" :scramble="scramble" />
+        </div>
+      </template>
+      <template #player1>
+        <div class="flex place-content-center">
+          <scramble-display event="333" :scramble="scramble" />
+        </div>
+      </template>
+    </TwoSideModal>
   </div>
 </template>

8673e70a28c3a3864bcf6e137ebc48b81e1c1114
Nikolay Masson <nick.masson@yandex.ru>
2025-03-27 22:15:01 +0100

    feat: added an undivided modal

---
+++ src/components/FullScreenModal.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const modal = ref<HTMLDialogElement>()
+
+defineExpose({
+  modal
+})
+</script>
+
+<template>
+  <dialog id="modal" class="modal" ref="modal">
+    <div class="modal-box" @click.stop="modal?.close()">
+      <slot name="modal-content"></slot>
+    </div>
+    <form method="dialog" class="modal-backdrop">
+      <button>close</button>
+    </form>
+  </dialog>
+</template>

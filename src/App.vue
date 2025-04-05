8197a4de95aeae5f4954da17c4655841566b4899
Nikolay Masson <nick.masson@yandex.ru>
2025-04-05 22:23:37 +0200

    feat: add prompt to add app to home screen on iOS

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <script setup lang="ts">
-import { onMounted, useTemplateRef } from 'vue';
+import { computed, onMounted, useTemplateRef, ref } from 'vue';
 import FullScreenModal from './components/FullScreenModal.vue';
 import PWABadge from './components/PWABadge.vue'
+import { useStorage } from '@vueuse/core';
 
 const noTouchModal = useTemplateRef('noTouchModal')
 
-onMounted(() => {
-  if (('ontouchstart' in window) ||
-    (navigator.maxTouchPoints > 0))
-    return
-  noTouchModal.value?.modal?.showModal()
+const isIos = computed(() => {
+  return /iPad|iPhone|iPod/.test(navigator.userAgent)
 })
+
+const pwaPromptKey = 'doNotShowPwaPromptAgain'
+const showPwaPrompt = ref(true)
+const doNotShowPwaPromptAgain = useStorage(pwaPromptKey, false, localStorage)
+function closePwaPrompt() {
+  showPwaPrompt.value = false
+}
+
+onMounted(() => {
+  if (!('ontouchstart' in window) &&
+    !(navigator.maxTouchPoints > 0)) {
+
+    noTouchModal.value?.modal?.showModal()
+  }
+  if (doNotShowPwaPromptAgain.value) {
+    showPwaPrompt.value = false
+  }
+})
+
 </script>
 
 <template>
@@ -26,5 +43,24 @@ onMounted(() => {
     </p>
     <p>This timer cannot be properly used without touch screen support</p>
   </FullScreenModal>
+  <div v-if="showPwaPrompt && isIos" class="toast toast-middle toast-center m-0" aria-labelledby="toast-message"
+    role="alert">
+    <div class="alert alert-info">
+      <div>
+        <h3 class="font-bold text-lg">Add a shortcut to Home Screen</h3>
+        <p>You can add this app to Home Screen! <br /> Here's how to do it on Safari:</p>
+        <ul class="list-disc list-inside">
+          <li>Touch the "Share" button in the bottom of screen</li>
+          <li>Touch "Add to Home Screen" and follow the instructions</li>
+        </ul>
+        <label class="flex items-center gap-2 mt-2">
+          <input type="checkbox" id="show-pwa-prompt" class="checkbox checkbox-neutral"
+            v-model="doNotShowPwaPromptAgain" />
+          <span>Don't show this again</span>
+        </label>
+        <button @click="closePwaPrompt" class="mt-3 btn shadow-md">Close</button>
+      </div>
+    </div>
+  </div>
   <PWABadge />
 </template>
--- src/lib/puzzlesMap.ts
+++ src/lib/puzzlesMap.ts
@@ -2,85 +2,85 @@ export const puzzlesMap: Record<string, EventContext> = {
   '2x2x2': {
     eventId: '222',
     displayName: '2x2x2',
-    scrambleSize: 'text-xl',
+    scrambleSize: 'text-xl md:text-2xl',
     generateScramble: true
   },
   '3x3x3': {
     eventId: '333',
     displayName: '3x3x3',
-    scrambleSize: 'text-xl',
+    scrambleSize: 'text-xl md:text-2xl',
     generateScramble: true
   },
   '4x4x4': {
     eventId: '444',
     displayName: '4x4x4',
-    scrambleSize: 'text-lg',
+    scrambleSize: 'text-lg md:text-xl',
     generateScramble: true
   },
   '5x5x5': {
     eventId: '555',
     displayName: '5x5x5',
-    scrambleSize: 'text-md',
+    scrambleSize: 'text-md md:text-lg',
     generateScramble: true
   },
   '6x6x6': {
     eventId: '666',
     displayName: '6x6x6',
-    scrambleSize: 'text-sm',
+    scrambleSize: 'text-sm md:text-md',
     generateScramble: true
   },
   '7x7x7': {
     eventId: '777',
     displayName: '7x7x7',
-    scrambleSize: 'text-sm',
+    scrambleSize: 'text-sm md:text-md',
     generateScramble: true
   },
   Pyraminx: {
     eventId: 'pyram',
     displayName: 'Pyraminx',
-    scrambleSize: 'text-xl',
+    scrambleSize: 'text-xl md:text-2xl',
     generateScramble: true
   },
   'Square-1': {
     eventId: 'sq1',
     displayName: 'Square-1',
-    scrambleSize: 'text-xl',
+    scrambleSize: 'text-xl md:text-2xl',
     generateScramble: true
   },
   Skewb: {
     eventId: 'skewb',
     displayName: 'Skewb',
-    scrambleSize: 'text-xl',
+    scrambleSize: 'text-xl md:text-2xl',
     generateScramble: true
   },
   Clock: {
     eventId: 'clock',
     displayName: 'Clock',
-    scrambleSize: 'text-md',
+    scrambleSize: 'text-md md:text-lg',
     generateScramble: true
   },
   Megaminx: {
     eventId: 'minx',
     displayName: 'Megaminx',
-    scrambleSize: 'text-xs',
+    scrambleSize: 'text-xs md:text-md',
     generateScramble: true
   },
   '6x6x6 (No scramble)': {
     eventId: '666',
     displayName: '6x6x6 (No scramble)',
-    scrambleSize: 'text-sm',
+    scrambleSize: 'text-sm md:text-md',
     generateScramble: false
   },
   '7x7x7 (No scramble)': {
     eventId: '777',
     displayName: '7x7x7 (No scramble)',
-    scrambleSize: 'text-sm',
+    scrambleSize: 'text-sm md:text-md',
     generateScramble: false
   },
   'Megaminx (No scramble)': {
     eventId: 'minx',
     displayName: 'Megaminx (No scramble)',
-    scrambleSize: 'text-xs',
+    scrambleSize: 'text-sm md:text-md',
     generateScramble: false
   }
 }
